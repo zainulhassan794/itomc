470bbaa21a93c4f94c6f4a1abc6cdc4cfcf1887d
zainulhassan794 <73742507+zainulhassan794@users.noreply.github.com>
2023-09-02 09:36:20 -0700

    Update settings.py

--- itomc/settings.py
+++ itomc/settings.py
@@ -136,10 +136,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
